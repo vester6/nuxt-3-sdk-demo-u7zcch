7d60100947094c74fafd6c49bfd0bfb96c55250e
vester6 <mave@tv2.dk>
2023-05-20 18:26:44 +0200

    Initial commit

---
+++ .nuxt/dist/server/server.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///home/projects/nuxt-3-sdk-demo-u7zcch/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
---
+++ README.md
@@ -0,0 +1,3 @@
+# nuxt-3-sdk-demo-u7zcch
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nuxt-3-sdk-demo-u7zcch)
